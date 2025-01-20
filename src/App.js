5c78b05875ac6d48a84d393416ff96f21f70ba7e
Andre-Luis-Aguiar <andreluisaguiar2000@gmail.com>
2025-01-20 12:38:37 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Bem-vindo à Biblioteca Digital</h1>
+      <p>Explore nosso acervo de livros e descubra suas próximas leituras favoritas.</p>
+    </div>
+  );
+}
+
+export default App;
